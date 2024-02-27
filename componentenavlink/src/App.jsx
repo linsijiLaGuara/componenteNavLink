62f9976821ef7033791b59b5a799df96bb79ada1
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-02-27 14:17:47 -0300

    add: final del ejemplo

--- componentenavlink/src/App.jsx
+++ componentenavlink/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbarlink from "./componente/Navbarlink";
 import Home from "./views/Home";
@@ -6,12 +6,12 @@ import Personajes from "./views/Personajes";
 
 export default function App() {
   return (
-    <div className="App">
+    <>
       <Navbarlink />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/personajes/:id" element={<Personajes />} />
       </Routes>
-    </div>
+    </>
   );
 }
--- componentenavlink/src/componente/Navbarlink.jsx
+++ componentenavlink/src/componente/Navbarlink.jsx
@@ -2,19 +2,22 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbarlink.css";
+
 export default function Navbarlink() {
   const setActiveClass = ({ isActive }) => (isActive ? "active" : undefined);
+
   return (
     <>
       <Navbar className="bg-navb">
         <Container>
-          <NavLink className={setActiveClass} to="/">
-            Home
-          </NavLink>
-          {"-"}
-          <NavLink className={setActiveClass} to="/personajes">
-            Personajes
-          </NavLink>
+          <Navbar.Collapse id="navbar-nav">
+            <NavLink className={`nav-link ${setActiveClass}`} to="/">
+              Home
+            </NavLink>
+            <NavLink className={`nav-link ${setActiveClass}`} to="/personajes">
+              Personajes
+            </NavLink>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
